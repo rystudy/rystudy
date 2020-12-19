979cd07ae442033562f4b7dd136f047c2137e208
rystudy <lockey0223@gmail.com>
2020-12-19 21:17:06 +0800

    增加地址

--- update/hostapi.js
+++ update/hostapi.js
@@ -1,4 +1,5 @@
 {
      "api_url": "https://api.rylearn.xyz",
-     "bakup_url": "https://apib.rylearn.xyz"
+     "bakup_url": "https://apib.rylearn.xyz",
+     "bakup2_url":"https://apic.rylearn.xyz"
 }
